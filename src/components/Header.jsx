f8e90261748930b0a3a47feaf3147777651b10fb
marcos-mus1 <marcos.musafiri@zabibu.co>
2022-06-02 18:48:01 +0200

    feat : add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import {
+  Badge,
+  Container,
+  Dropdown,
+  FormControl,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+
+const Header = () => {
+  const {
+    state: { cart },
+  } = CartState();
+  return (
+    <Navbar
+      bg="dark"
+      variant="dark"
+      style={{ height: 80, position: "sticky", top: 0, zIndex: 999 }}
+    >
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">Shopping Cart</Link>
+        </Navbar.Brand>
+        <Navbar.Text className="search">
+          <FormControl
+            style={{ width: 500 }}
+            placeholder="Search"
+            className="m-auto"
+          />
+        </Navbar.Text>
+        <Nav>
+          <Dropdown alignright="true">
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{cart.length}</Badge>
+            </Dropdown.Toggle>
+            <Dropdown.Menu style={{ minWidth: 370 }}>
+              <span style={{ padding: 10 }}>Cart is Empty</span>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
