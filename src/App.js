9737e8310e53989088baef2099885917c555f280
marcos-mus1 <marcos.musafiri@zabibu.co>
2022-06-02 18:52:46 +0200

    project init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
